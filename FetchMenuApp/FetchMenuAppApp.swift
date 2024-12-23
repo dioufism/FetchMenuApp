8fa38283634f44ed52946d44b9b4c18775fe1494
Ousmane <dioufism@gmail.com>
2024-12-22 19:40:18 -0600

    Initial Commit

---
+++ FetchMenuApp/FetchMenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchMenuAppApp.swift
+//  FetchMenuApp
+//
+//  Created by ousmane diouf on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchMenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
